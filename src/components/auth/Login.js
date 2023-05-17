2b829c38326ef5be0d96d65c7118657d1c561fe1
tuan194492 <tuan1216302001@gmail.com>
2023-05-17 17:16:19 +0700

    Login Signup Logout

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -6,6 +6,7 @@ import ROLE from "../../constant/ROLE";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
   const [role, setRole] = useState(ROLE.NGUOI_CHO_THUE)
 
   const navigate = useNavigate();
@@ -30,22 +31,39 @@ const Login = () => {
     <div>
       <form onSubmit={handleLogin}>
         <h3>Login</h3>
-        <input
-          type="text"
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <div className="row col-3 mb-1">
+          <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        
+        <div className="row col-3 mb-1">
         <input
           type="password"
           placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <div onChange={onChangeRole}>
+        </div>
+
+        <div className="row col-3 mb-1">
+        <input
+          type="text"
+          placeholder="name"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        </div>
+        
+        
+        <div onChange={onChangeRole} >
           <input type="radio" value={ROLE.NGUOI_CHO_THUE} name="role"/> Người cho thuê
           <input type="radio" value={ROLE.NGUOI_THUE} name="role"/> Người thuê
-      </div>
+        </div>
+
 
         <button type="submit">Log in</button>
       </form>
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import authService from "../../services/AuthServices";
-const Login = () => {
+import ROLE from "../../constant/ROLE";
+import Button from 'react-bootstrap/Button';
+
+
+const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [role, setRole] = useState(ROLE.NGUOI_CHO_THUE)
+  const [birtdDay, setBirthDay] = useState("");
+  const [name, setName] = useState("");
 
   const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
+  const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      await authService.login(email, password).then(
+      await authService.signup(e.target.value).then(
         () => {
           navigate("/home");
           window.location.reload();
@@ -23,27 +30,62 @@ const Login = () => {
       console.log(err);
     }
   };
+  const onChangeRole = (e) => {
+    setRole(e.target.value);
+  }
 
   return (
     <div>
-      <form onSubmit={handleLogin}>
-        <h3>Login</h3>
-        <input
-          type="text"
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+      <form onSubmit={handleSignup}>
+        <h3>Sign up</h3>
+        <div className="row col-3 mb-1">
+          <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        
+        <div className="row col-3 mb-1">
         <input
           type="password"
           placeholder="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Log in</button>
+
+        </div>
+
+        <div className="row col-3 mb-1">
+        <input
+          type="text"
+          placeholder="name"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        </div>
+
+        <div className="row col-3 mb-1">
+        <input
+          type="date"
+          placeholder="birth"
+          value={birtdDay}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        </div>
+        
+        
+        <div onChange={onChangeRole} >
+          <input type="radio" value={ROLE.NGUOI_CHO_THUE} name="role"/> Người cho thuê
+          <input type="radio" value={ROLE.NGUOI_THUE} name="role"/> Người thuê
+        </div>
+
+        <Button variant="success">Sign up</Button>
       </form>
+      
     </div>
   );
 };
 
-export default Login;
+export default Signup;
